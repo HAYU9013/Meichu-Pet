8e7303e3877b7358f7b76c196b80dc4348270e1b
HAYU9013 <114861748+HAYU9013@users.noreply.github.com>
2024-10-25 20:21:46 +0800

    update package

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,17 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/TSMC-pet/" : "/",
+  
+    pwa: {
+      name: 'My Vue App',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black',
+      manifestOptions: {
+        background_color: '#ffffff'
+      }
+    }
+
+  
 });
